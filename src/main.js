acc65a0db7ad0381a6a537f216abc08dbc07c85f
liuaiying <1425577366@qq.com>
2022-07-05 12:18:55 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-// 1.0 样式引入
-// 在素材的文件下 直接拖进来就可以了
-import './assets/styles/base.css';
-import './assets/styles/index.css';
-
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
