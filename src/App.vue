5dd80fd016741f36e76d533bda9236c32d954bee
liuaiying <1425577366@qq.com>
2022-07-06 20:41:02 +0800

    注册

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="box">
+      <button @click="phone">手机注册</button>
+      <button @click="email">邮箱注册</button>
+    </div>
+
+    <div>
+      <keep-alive>
+        <component :is="comName">
+          <template v-slot:phone>
+            <input type="text" placeholder="输入手机号" />
+          </template>
+          <template v-slot:yzh>
+            <input type="text" placeholder="输入验证码" />
+          </template>
+
+          <template v-slot:inputPassword>
+            <input type="password" placeholder="输入密码" />
+          </template>
+
+          <template v-slot:confirmPassword>
+            <input type="password" placeholder="再次输入密码" />
+          </template>
+          <template v-slot:inputText>
+            <input type="text" placeholder="输入邀请码" />
+          </template>
+        </component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyPhone from './components/MyPhone.vue';
+import MyEmail from './components/MyEmail.vue';
+export default {
+  data() {
+    return {
+      comName: 'MyPhone',
+      active: '',
+    };
+  },
+  methods: {
+    phone() {
+      this.comName = 'MyPhone';
+      this.isAll = true;
+    },
+    email() {
+      this.comName = 'MyEmail';
+      this.isAll = true;
+    },
+  },
+  components: {
+    MyPhone,
+    MyEmail,
+  },
+};
+</script>
+
+<style>
+.active {
+  border-bottom: 3px solid blue;
+}
+
+.box {
+  width: 100%;
+  height: 45px;
+  color: #ccc;
+  display: flex;
+  background-color: pink;
+}
+button {
+  width: 50%;
+  border: transparent;
+}
+.left {
+  flex: 1;
+}
+.right {
+  flex: 1;
+}
+</style>
---
+++ src/components/MyEmail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>选择所在地区（实名认证主体所在地）</span><br />
+
+    <span>手机号:</span>
+    <slot name="phone"></slot>
+    <br /><br />
+
+    <slot name="yzh"></slot><br /><br />
+
+    <span>登陆密码:</span>
+    <br />
+    <slot name="inputPassword"></slot><br /><br />
+    <slot name="confirmPassword"></slot><br /><br />
+
+    <span>邀请码（选填）</span>
+    <slot name="inputText"></slot><br /><br />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+input {
+  width: 100%;
+  height: 30px;
+  color: #ccc;
+  background-color: yellowgreen;
+}
+</style>
