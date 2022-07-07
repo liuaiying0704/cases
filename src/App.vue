71a5f9e4156bea51d33a92a5ead2cd21171464f1
liuaiying <1425577366@qq.com>
2022-07-07 20:58:25 +0800

    gengxin

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <MyHeader :background="blue" title="tabbar" :tabList="tabList"></MyHeader>
+    <div class="main">
+      <component :is="comName"> </component>
+    </div>
+    <MyTabBar></MyTabBar>
+  </div>
+</template>
+
+<script>
+import MyTabBar from './components/MyTabBar.vue';
+import MyHeader from './components/MyHeader.vue';
+export default {
+  components: {
+    MyTabBar,
+    MyHeader,
+  },
+  data() {
+    return {
+      tabList: [
+        // 底部导航的数据
+        {
+          iconText: 'icon-shangpinliebiao',
+          text: '商品列表',
+          componentName: 'MyGoodsList',
+        },
+        {
+          iconText: 'icon-sousuo',
+          text: '商品搜索',
+          componentName: 'MyGoodsSearch',
+        },
+        {
+          iconText: 'icon-user',
+          text: '我的信息',
+          componentName: 'MyUserInfo',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/MyGoodsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import MyTable from '../components/MyTable.vue';
+export default {
+  components: {
+    MyTable,
+  },
+};
+</script>
+
+<style></style>
