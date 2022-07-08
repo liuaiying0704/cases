8f9ea2ce2d03aded694a3a8f7f7c2b47ee24fa80
liuaiying <1425577366@qq.com>
2022-07-08 14:46:37 +0800

    vant组件库的使用

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,63 @@
 <template>
   <div>
-    <MyHeader :background="blue" title="tabbar" :tabList="tabList"></MyHeader>
-    <div class="main">
-      <component :is="comName"> </component>
-    </div>
-    <MyTabBar></MyTabBar>
+    <van-button type="primary" @click="btn">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <hr />
+
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
-import MyTabBar from './components/MyTabBar.vue';
-import MyHeader from './components/MyHeader.vue';
+// 方式二
+// import Button from 'vant/lib/button';
+// import 'vant/lib/button/style';
+// export default {
+//   components: {
+//     VanButton: Button,
+//     // [Button.name]: Button,
+//   },
+// };
+import { Dialog } from 'vant';
 export default {
-  components: {
-    MyTabBar,
-    MyHeader,
-  },
   data() {
     return {
-      tabList: [
-        // 底部导航的数据
-        {
-          iconText: 'icon-shangpinliebiao',
-          text: '商品列表',
-          componentName: 'MyGoodsList',
-        },
-        {
-          iconText: 'icon-sousuo',
-          text: '商品搜索',
-          componentName: 'MyGoodsSearch',
-        },
-        {
-          iconText: 'icon-user',
-          text: '我的信息',
-          componentName: 'MyUserInfo',
-        },
-      ],
+      username: '',
+      password: '',
     };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values);
+    },
+    btn() {
+      Dialog({ message: '提示', showCancelButton: true });
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
-
 Vue.config.productionTip = false;
+
+// 方式三，只需下面的配置
+// import Vant from 'vant';
+// import 'vant/lib/index.css';
+// Vue.use(Vant);
+
+// 方式1，，1、下载npm i babel-plugin-import -D   2、babel.config.js配置   3、下面引入
+// 接着你可以在代码中直接引入 Vant 组件
+// 插件会自动将代码转化为方式二中的按需引入形式
+import { Button, Form, Field } from 'vant';
+Vue.use(Button);
+Vue.use(Form);
+Vue.use(Field);
 
 new Vue({
   render: (h) => h(App),
